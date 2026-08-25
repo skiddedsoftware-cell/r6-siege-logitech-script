57bb00768e1aaab966fb4178e0217b13d923f577
skiddedsoftware-cell <colbyleeps4@gmail.com>
2026-08-24 22:21:40 -0500

    Rewrite script using proper Logitech GHub API syntax

--- r6_siege_script.lua
+++ r6_siege_script.lua
@@ -2,27 +2,25 @@
 -- Dual Mouse Button Activation (Left + Right Click)
 -- DPI: 800 | Multiplier: 0.00223 | Sensitivity: 53/53
 
-local leftMouseDown = false
-local rightMouseDown = false
+EnableScript = true
+DPI = 800
+MULTIPLIER = 0.00223
+SENSITIVITY = 53
+DelayRate = 7 -- in milliseconds
+
+EnablePrimaryMouseButtonEvents(true)
 
 function OnEvent(event, arg)
-    if event == "MOUSE_BUTTON_PRESSED" then
-        if arg == 1 then
-            leftMouseDown = true
-        elseif arg == 3 then
-            rightMouseDown = true
-        end
-    elseif event == "MOUSE_BUTTON_RELEASED" then
-        if arg == 1 then
-            leftMouseDown = false
-        elseif arg == 3 then
-            rightMouseDown = false
-        end
-    elseif event == "MOUSE_MOVE" then
-        if leftMouseDown and rightMouseDown then
-            -- Both buttons pressed - apply movement
-            local x, y = arg, 0
-            MoveMouseRelative(x, y)
+    if EnableScript ~= false then
+        if IsMouseButtonPressed(3) then -- Right mouse button
+            repeat
+                if IsMouseButtonPressed(1) then -- Left mouse button
+                    repeat
+                        MoveMouseRelative(MULTIPLIER * 100, 0)
+                        Sleep(DelayRate)
+                    until not IsMouseButtonPressed(1)
+                end
+            until not IsMouseButtonPressed(3)
         end
     end
 end
